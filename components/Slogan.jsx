39520f26df11d92277d1f648f4f798b39094961b
Minh Tin Tang <minhtintang@gmail.com>
2022-03-20 17:30:07 +0700

    change sologan

--- components/Slogan.jsx
+++ components/Slogan.jsx
@@ -2,11 +2,11 @@ import React from "react";
 import styles from "../styles/Slogan.module.css";
 
 const Slogan = () => {
-    return (
-        <div>
-            <p className={styles.slogan}>GAME HOT NHẤT VIỆT NAM</p>
-        </div>
-    );
+  return (
+    <div>
+      <p className={styles.slogan}>Play and Enjoy</p>
+    </div>
+  );
 };
 
 export default Slogan;
